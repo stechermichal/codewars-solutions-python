e4c5f1a44dca69ab22dd463dac1b771380eb6f48
MichalS <stechermichal@gmail.com>
2021-04-04 21:57:34 +0200

    Added 8 kyu kata.

--- 8_kyu/fake_binary_8_kyu.py
+++ 8_kyu/fake_binary_8_kyu.py
@@ -2,4 +2,12 @@
 Return the resulting string."""
 
 
+def fake_bin(x):
+    x = [int(i) for i in x]
+    for i, value in enumerate(x):
+        if value < 5:
+            x[i] = 0
+        else:
+            x[i] = 1
+    return ''.join(str(i) for i in x)
 
